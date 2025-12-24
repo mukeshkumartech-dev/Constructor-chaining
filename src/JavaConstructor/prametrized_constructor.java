25f84f5b9d52d746dd6d9d743cc5e6614a4b3036
mukeshkumartech-dev <mukeshkumartech.dev@gmail.com>
2025-12-24 19:17:46 +0530

    update commit

---
+++ src/JavaConstructor/prametrized_constructor.java
@@ -0,0 +1,25 @@
+package JavaConstructor;
+class cons{
+	String name;
+	int age;
+	long contact;
+
+public cons(String name, int age, long contact) {
+	
+		this.name=name;
+		this.age =age;
+		this.contact=contact;
+		
+	}
+	public void display() {
+		System.out.println("Name:-"+name+" "+"Age:-"+age+" "+"Contact:-"+contact);
+	}
+}
+public class prametrized_constructor {
+
+	public static void main(String[] args) {
+		cons c1 =new cons("mukesh",20,9345536911l);
+c1.display();
+	}
+
+}
