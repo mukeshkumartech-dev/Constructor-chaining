53343a273427a2f2e77d9b908713d43c982461ac
mukeshkumartech-dev <mukeshkumartech.dev@gmail.com>
2025-12-26 14:59:46 +0530

    non perametrized constructor commit

---
+++ src/JavaConstructor/non_perametrized.java
@@ -0,0 +1,12 @@
+package JavaConstructor;
+
+public class non_perametrized {
+
+    non_perametrized() {
+        System.out.println("Non-parameterized constructor called");
+    }
+
+    public static void main(String[] args) {
+        new non_perametrized();
+    }
+}
